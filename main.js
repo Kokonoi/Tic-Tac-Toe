187ca9e26be196b4e15ec430b3f2e0fde39c35c7
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-07-24 19:03:39 +0200

    displayController module

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ const gameBoard = (() => {
 
   return getBoard;
 })();
+
+const displayController = (() => {
+  const cells = document.querySelectorAll(".cell");
+
+  const renderBoard = () => {
+    const board = gameBoard.getBoard();
+    cells.forEach((element, index) => {
+      element.classList.add(board[index]);
+    });
+  };
+  return { renderBoard };
+})();
