2f012ff2217bac83bc3b795ffdcb05e9b1342cb7
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-07-25 18:49:50 +0200

    added a clickHandle

--- main.js
+++ main.js
@@ -13,7 +13,24 @@ const Player = (playerName, symbol) => {
 const DisplayController = (() => {
   const cells = document.querySelectorAll(".cell");
 
+  //pust symbols and checks if a cell already has a symbol
+  const putSymbol = (cell, symbol) => {
+    if (cell.classList.contains(symbol)) {
+      return;
+    } else cell.classList.add(symbol);
+  };
+
+  //adds event listeners
+  const clickHandle = () => {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        putSymbol(cell, "x");
+      });
+    });
+  };
+
   const renderBoard = () => {
+    clickHandle();
     const board = gameBoard.getBoard();
     cells.forEach((element, index) => {
       const className = board[index];
@@ -22,10 +39,7 @@ const DisplayController = (() => {
       }
     });
   };
-
-  const clickHandle = () => {};
-
-  return { renderBoard };
+  return { renderBoard, clickHandle };
 })();
 
 DisplayController.renderBoard();
